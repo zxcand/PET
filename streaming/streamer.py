7520485bf06d0e4e58ff6566fda650f03ea2ac5a
Fan Wang <fan@Fans-MacBook-Pro.local>
2015-06-08 21:41:07 +0800

    add threading, and fix bugs in streamer class

--- streaming/streamer.py
+++ streaming/streamer.py
@@ -6,20 +6,39 @@ from PIL import Image, ImageTk #Python Image Libraries, required for displaying 
 
 import cv2
 import numpy as np
+import threading
 
-DEBUG  = True
-OUTPUT = True
+DEBUG  = False
+OUTPUT = False
 
 HOST  = '192.168.1.54'
 PORT  = 8080
 QUERY = '/?action=snapshot'
 
 
-class Streamer:
+class Streamer(threading.Thread):
 	def __init__(self, _host=HOST, _port=PORT, _query=QUERY):
+		super(Streamer, self).__init__()  
 		self.host = _host
 		self.port = _port
 		self.query = _query
+		self.I = None
+
+		#start the reciving stream
+		thread = threading.Thread(target=self.run, args=())
+		thread.daemon = True # Daemonize thread
+		thread.start()  
+
+	def run(self):
+		while True:
+			self.I = self.getStreamFromHost()
+			if OUTPUT:
+				img =Image.open(StringIO.StringIO(self.I)) #convert to jpeg object from the stream
+				img = np.array(img)
+				img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+				cv2.imshow("streaming client",img)
+				cv2.waitKey(1)
+
 
 	def getStreamFromHost(self):
 		h = httplib.HTTP(self.host, self.port)
@@ -46,10 +65,15 @@ class Streamer:
 		raw = self.getStreamFromHost()
 		img =Image.open(StringIO.StringIO(raw)) #convert to jpeg object from the stream
 		img = np.array(img)
-		img = cv2.cvtColor(I,cv2.COLOR_BGR2RGB)
+		img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
 		if OUTPUT:
 			cv2.imshow("streaming client",img)
 			cv2.waitKey(1)
 		return img
+
+if __name__=="__main__":
+	s = Streamer()
+	while True:
+		s.getFrame()
 
